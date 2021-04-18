f4450fb2d1cac84885a727283aa27e0ccc01784d
Veiasai <o206@qq.com>
2021-04-18 03:01:03 +0000

    test

--- standard/11/test/type_traits_test.cc
+++ standard/11/test/type_traits_test.cc
@@ -1,1 +1,17 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include <type_traits/type_traits.hpp>
+
+namespace cpp::std::test {
+using namespace testing;
+
+class TypeTraitsTest : public Test {};
+
+TEST_F(TypeTraitsTest, IsSame) {
+    EXPECT_EQ((is_same<int, int>()), true);
+    EXPECT_EQ((is_same<int, float>()), false);
+    EXPECT_EQ((is_same<int&, int&&>()), false);
+}
+
+}  // namespace cpp::std::test
