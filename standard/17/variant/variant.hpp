289f15a0d70297cb0c231f58fef535ce40dadea3
Veiasai <o206@qq.com>
2021-07-23 07:51:15 +0800

    default ctor

--- standard/17/variant/variant.hpp
+++ standard/17/variant/variant.hpp
@@ -16,7 +16,7 @@ struct variant_traits {
         if (id == typeid(T).hash_code())
             reinterpret_cast<T*>(data)->~T();
         else if constexpr (sizeof...(Ts) == 0)
-            return;
+            throw std::runtime_error("Unexpected");
         else
             variant_traits<Ts...>::destroy(id, data);
     }
@@ -25,7 +25,7 @@ struct variant_traits {
         if (old_t == typeid(T).hash_code())
             new (new_v) T(std::move(*reinterpret_cast<T*>(old_v)));
         else if constexpr (sizeof...(Ts) == 0)
-            return;
+            throw std::runtime_error("Unexpected");
         else
             variant_traits<Ts...>::move(old_t, old_v, new_v);
     }
@@ -34,7 +34,7 @@ struct variant_traits {
         if (old_t == typeid(T).hash_code())
             new (new_v) T(*reinterpret_cast<const T*>(old_v));
         else if constexpr (sizeof...(Ts) == 0)
-            return;
+            throw std::runtime_error("Unexpected");
         else
             variant_traits<Ts...>::copy(old_t, old_v, new_v);
     }
@@ -42,22 +42,32 @@ struct variant_traits {
 
 }  // namespace
 
-template <typename... Ts>
+template <typename T_0, typename... Ts>
 class variant {
    public:
-    variant() = default;
-    variant(const variant& rhs) = default;
-    variant(variant&& rhs) = default;
-    variant& operator=(const variant& rhs)
-    {
-        if(rhs.mTypeID == 0)
-        {
+    template <typename Require =
+                  std::enable_if_t<std::is_default_constructible_v<T_0>>>
+    variant() {
+        mTypeID = typeid(T_0).hash_code();
+        new (&mData) T_0();
+    };
+    variant(const variant& rhs) { *this = rhs; };
+    variant(variant&& rhs) {
+        if (rhs.mTypeID == 0) {
             destroy();
+        } else {
+            _traits::move(mTypeID, &rhs.mData, &mData);
+            rhs.mTypeID = 0;
         }
-        else
-        {
+        return *this;
+    };
+    variant& operator=(const variant& rhs) {
+        if (rhs.mTypeID == 0) {
+            destroy();
+        } else {
             _traits::copy(mTypeID, &rhs.mData, &mData);
         }
+        return *this;
     };
 
     variant& operator=(variant&& rhs) {
@@ -70,7 +80,7 @@ class variant {
     };
     ~variant() { destroy(); };
 
-    using _traits = variant_traits<Ts...>;
+    using _traits = variant_traits<T_0, Ts...>;
 
     template <typename T,
               typename = std::enable_if_t<_traits::template contains<T>>>
@@ -97,8 +107,8 @@ class variant {
             mTypeID = 0;
         }
     }
-    static const size_t data_size = std::max({size_t(0), sizeof(Ts)...});
-    static const size_t data_align = std::max({size_t(0), alignof(Ts)...});
+    static const size_t data_size = std::max({sizeof(T_0), sizeof(Ts)...});
+    static const size_t data_align = std::max({alignof(T_0), alignof(Ts)...});
 
     using data_t = typename std::aligned_storage<data_size, data_align>::type;
 
